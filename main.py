5ff4867e6ff4f2ced683d1c4e4088da62ba8961f
Pavel <jobproductmail@gmail.com>
2025-02-23 15:41:17 +0300

    creation schema with data

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ with open('config.json', encoding='UTF-8') as f:
 
 conn = psycopg2.connect(**db_config)
 cursor = conn.cursor()
+
+cursor.execute('drop schema if exists BANK cascade;')
+conn.commit()
+cursor.execute('create schema if not exists BANK;')
+conn.commit()
+
+cursor.execute('set search_path to bank;')
+
+with open('sql_scripts/ddl_dml.sql', 'r', encoding='UTF-8') as f:
+    loading_script = f.read()
+
+for part in loading_script.split(';'):
+    part = part.strip()
+    if part:
+        cursor.execute(part)
+        conn.commit()
