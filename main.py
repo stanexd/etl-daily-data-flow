9786de14303b72b1c3a2206dad1ca2c9e4eee768
Pavel <jobproductmail@gmail.com>
2025-02-28 18:33:29 +0300

    add tables of deleted_rows

--- main.py
+++ main.py
@@ -170,5 +170,43 @@ def create_new_rows_passport_blacklist():
 create_new_rows_terminals()
 create_new_rows_passport_blacklist()
 
+#Создание временных таблиц для удаленных строк
+##################################################################################################################################################################
 
+def create_deleted_rows_terminals():
 
+    cursor.execute('drop table if exists stg_deleted_rows_terminals;')
+    conn.commit()
+
+    cursor.execute(
+        '''
+        create table stg_deleted_rows_terminals as
+            select t2.terminal_id,
+                   t2.terminal_type,
+                   t2.terminal_city,
+                   t2.terminal_address
+            from stg_terminals as t1
+            right join DWH_DIM_TERMINALS_HIST as t2
+            on t1.terminal_id = t2.terminal_id
+            where t1.terminal_id is null
+        ''')
+    conn.commit()
+
+def create_deleted_rows_passport_blacklist():
+
+    cursor.execute('drop table if exists stg_deleted_rows_passport_blacklist')
+
+    cursor.execute(
+        '''
+        create table stg_deleted_rows_passport_blacklist as
+            select t2.effective_from,
+                   t2.passport
+            from stg_passport_blacklist as t1
+            right join DWH_DIM_PASSPORT_BLACKLIST_HIST as t2
+            on t1.passport = t2.passport
+            where t1.passport is null
+        ''')
+    conn.commit()
+
+create_deleted_rows_terminals()
+create_deleted_rows_passport_blacklist()
